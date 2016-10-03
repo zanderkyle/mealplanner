d90ec4a89088c3cd4295ae6dc3519f5d3af495e1
Kyle Gonyea <kgonyea@zandergraphics.net>
2016-10-02 23:48:14 -0700

    start admin controller code

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,11 @@
+@include('includes.admin_header')
+
+        <div class="container">
+            <div class="content">
+                <div class="title">Admin Page</div>
+            </div>
+        </div>
+    </body>
+</html>
+
+@include('includes.admin_footer')
---
+++ resources/views/includes/admin_header.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ASD Meal Planning Website</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: block;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+
+        <div id="admin_menu">Home | Food List</div>
+
+    </head>
+
+    <body>
+
+
